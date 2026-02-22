5ea9497a76ca991cf4902ed85cf66525eb07e3cf
serge1710 <serge.lhr@ik.me>
2026-02-22 10:50:23 +0100

    Update common.sh

--- app/scripts/common.sh
+++ app/scripts/common.sh
@@ -30,7 +30,7 @@ run_as_user() {
 
 # System package requirements for RatOS (read by Moonraker and scripts)
 # shellcheck disable=SC2034
-PKGLIST="python3-numpy python3-matplotlib curl git libopenblas-base"
+PKGLIST="python3-numpy python3-matplotlib curl git libopenblas-dev"
 
 report_status()
 {
